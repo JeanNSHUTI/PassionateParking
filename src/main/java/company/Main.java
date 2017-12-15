a39185fe959677227e9cef108ac6785b4706bd88
Benjamin Vandenbussche <benjvdb9@hotmail.com>
2017-12-15 15:01:57 +0100

    Push for database team

--- src/main/java/company/Main.java
+++ src/main/java/company/Main.java
@@ -1,24 +1,46 @@
 package company;
+
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Main {
     public static void main (String[] args){
         Parking park = Parking.getInstance();
         Size size = new Size(190, 450);
-        Place place = new Place(size);
-        park.addPlace(place);
-        ArrayList<Place> place_array = park.getPlaces();
-
-        String test = place_array.get(0).toString();
-        System.out.print(test);
-
-
-        Color color = new Color("black");
-        Color color2 = new Color("black");
 
         //Example to decorate a vehicle.
-        Vehicle vehicle = new ColorDecorator(new ColorDecorator(new Car("ABC",  size),
-                color), color2);
-        System.out.print(vehicle.vehicleCharacteristic());
+        Color color = new Color("black");
+        Color color2 = new Color("black");
+        Vehicle vehicle = new ColorDecorator(new ColorDecorator(new Car("ABC",  size), color), color2);
+
+        for (int j = 0; j < 2; j++) {
+            Place place = new Place(size);
+            park.addPlace(place);
+        }
+
+        for (int j = 0; j < 2; j++) {
+            Place place = new Place(size);
+            place.setBusy(vehicle);
+            park.addPlace(place);
+        }
+
+        Place place = new Place(size);
+        Date date_test = new Date();
+        place.setArrival(date_test);
+        place.setDeparture(date_test);
+        System.out.print(place.getArrival());
+        System.out.print(place.getDeparture());
+
+        ArrayList<Place> place_array = park.getPlaces();
+        String test = place_array.get(0).toString();
+        System.out.print("\nALL PLACES\n");
+        System.out.print(place_array);
+
+        System.out.print("\nEMPTY PLACES\n");
+        System.out.print(park.getEmptyPlace());
+
+        System.out.print("\nBUSY PLACES\n");
+        System.out.print(park.getBusyPlaces());
+        //System.out.print(vehicle.vehicleCharacteristic());
     }
 }
