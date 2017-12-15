9a91dcb3bf5a35916114daf1b4dcec0bd6337ed4
jnshuti93 <jnshuti93@yahoo.com>
2017-12-15 16:21:18 +0100

    updating tests

--- src/test/java/test/ParkingTest.java
+++ src/test/java/test/ParkingTest.java
@@ -2,12 +2,15 @@ package test;
 
 import org.junit.Assert;
 import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 import company.*;
 
 public class ParkingTest {
 
     @Test
-    public void ParkingTest() {
+    public void AddPlaceTest() {
+
         //creation of a parking singleton
         Parking parking = Parking.getInstance();
         //creation of a size object in order to have a parameter for the place
@@ -20,21 +23,37 @@ public class ParkingTest {
         //test the newly changed places array
         Assert.assertEquals("should be 1",1, parking.getPlaces().size());
         //now testing the array listing how many busy and free places are in the parking
+    }
 
+    @Test
+    public void GetEmptyPlaceTest() {
+
+        //creation of a parking singleton
+        Parking parking = Parking.getInstance();
+
+        //Verify that the place added is free
         Assert.assertEquals("should be 1",1, parking.getEmptyPlace().size());
         Assert.assertEquals("should be 1",0, parking.getBusyPlaces().size());
-        int totalsize = parking.getEmptyPlace().size()+parking.getBusyPlaces().size();
+        int totalsize = parking.getEmptyPlace().size() + parking.getBusyPlaces().size();
         //test if sum of both arrays equals the size of the total places array
         Assert.assertEquals("should be true",parking.getPlaces().size(), totalsize);
+    }
+
+    @Test
+    public void GetBusyPlaceTest() {
+
+        //creation of a parking singleton
+        company.Parking parking = Parking.getInstance();
+
         //Car creation in order to occupy a place
         Size carsize = new Size(190,450);
         Car car = new Car("ABC123", carsize);
-        place.setBusy(car);
+        parking.getPlaces().get(0).setBusy(car);
         //retest the busy and free lists
         Assert.assertEquals("should be 1",0, parking.getEmptyPlace().size());
         Assert.assertEquals("should be 1",1, parking.getBusyPlaces().size());
 
-        totalsize = parking.getEmptyPlace().size()+parking.getBusyPlaces().size();
+        int totalsize = parking.getEmptyPlace().size()+parking.getBusyPlaces().size();
         Assert.assertEquals("should be true",parking.getPlaces().size(), totalsize);
     }
 }
